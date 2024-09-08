df874ec3595ecad9030cd3e6b699ac5c71de0e72
Pratyush Kumar Bisoyi <pratyushliku29@gmail.com>
2024-09-08 10:15:12 +0530

    logging issue solved

--- discord_points_bot.py
+++ discord_points_bot.py
@@ -2,6 +2,12 @@ import discord
 from discord.ext import commands
 import os
 import psycopg2
+import logging
+from difflib import get_close_matches
+
+# setup logging
+logging.basicConfig(level=logging.WARNING)
+
 
 # Define the intents your bot will use
 intents = discord.Intents.default()
@@ -11,6 +17,26 @@ intents.members = True
 # Initialize the bot with intents and a command prefix
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+# Handle invalid commands
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.content.startswith('!'):
+        command = message.content.split()[0][1:]  # Extract command
+        if command not in bot.commands:  # Check if command exists
+            logging.warning(f"Invalid command used: {command}")
+            close_matches = get_close_matches(command, bot.commands, n=1)
+            if close_matches:
+                suggested_command = close_matches[0]
+                await message.channel.send(f"Invalid command. Did you mean `!{suggested_command}`?")
+            else:
+                await message.channel.send("Invalid command. Please check your input.")
+    else:
+        await bot.process_commands(message)  # Ensure other commands are processed
+
+        
 # Connect to PostgreSQL database
 DATABASE_URL = os.getenv("DATABASE_URL")
 conn = psycopg2.connect(DATABASE_URL, sslmode="require")
@@ -47,13 +73,13 @@ def get_user_points_db(user_id):
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
-@bot.command()
+@bot.command(name='add_points', aliases=['add_point','addpoint','Add_point'])
 @commands.has_permissions(administrator=True)
 async def add_points(ctx, user: discord.Member, points: int):
     add_points_db(user.id, points)
     await ctx.send(f'{user.global_name} has been awarded {points} points!')
 
-@bot.command()
+@bot.command(name='leaderboard', aliases=['Leaderboard','Leader'])
 async def leaderboard(ctx):
     leaderboard_data = get_leaderboard_db()
     
@@ -68,7 +94,7 @@ async def leaderboard(ctx):
 
     await ctx.send(leaderboard_message)
 
-@bot.command()
+@bot.command(name='points', aliases=['point','Point'])
 async def points(ctx, user: discord.Member = None):
     # If no user is mentioned, use the command invoker
     if user is None:
